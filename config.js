0a1f7e0fd61ecc9f9bed74baa5020ce4a51416fa
gning22 <gning_mamadou8@icloud.com>
2026-02-13 01:39:28 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  prefix: ".",
+  owner: ["221763175367"], // Remplace par ton numéro complet
+  botname: "SHADOW MD BOT",
+  packname: "SHADOW KILLER",
+  author: "SHADOW KILLER"
+}
